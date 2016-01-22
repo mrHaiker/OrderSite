4325ce2d3a8df64b5cf8d87a71bc7c77c624568c
Haiker <mr.haiker@mail.ru>
2016-01-22 15:29:50 +0200

    push

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+    var $iconMenu =  $('#list-menu i');
+    var $iconContact =  $('#list-contact i');
+    var $contact = $('#btn-contact');
+    var $nav = $('#nav');
+    var $listServices = $('#list-services');
+
+    // Click on mobile menu btn
+    $('#list-menu').click(function () {
+
+        $iconMenu.toggleClass('close-btn');
+        changeIconMenu($iconMenu, 'fa-bars');
+
+        if ($contact.hasClass('open-list-user')) {    // уделение открытого меню
+            $contact.removeClass('open-list-user');
+            $iconContact.removeClass('close-btn');
+            changeIconMenu('#list-contact i', 'fa-user');
+        }
+
+        $('#nav').toggleClass('open-list-menu');    // открывает меню
+    });
+
+    // Click on mobile contact btn
+    $('#list-contact').click(function () {
+
+        $iconContact.toggleClass('close-btn');
+        changeIconMenu($iconContact, 'fa-user');
+
+        if ($nav.hasClass('open-list-menu')) {    // уделение открытого меню
+            $nav.removeClass('open-list-menu');
+            $iconMenu.removeClass('close-btn');
+            changeIconMenu('#list-menu i', 'fa-bars');
+        }
+
+        $('#btn-contact').toggleClass('open-list-user');   // открывает меню
+    });
+
+    // Click on service list-items
+    $('#list-services a').click(function (e) {
+        if ($listServices.hasClass('hidden-mobile')) e.preventDefault();    // Если список сервисов еще не открыт --> отменяеем переход по ссылке
+
+        $listServices.toggleClass('hidden-mobile');
+    })
+
+});
